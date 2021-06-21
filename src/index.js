f637e93d85ef900d152ed448b43b6a01cc1ff293
IsaacYou <isaac_you@mhand.net>
2021-06-21 20:46:27 +0900

    :add swiper

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import Focusable from './Focusable.jsx';
-import VerticalList from './VerticalList.jsx';
-import HorizontalList from './HorizontalList.jsx';
-import Grid from './Grid.jsx';
-import Navigation from './Navigation.jsx';
+// import Focusable from './Focusable.jsx';
+// import VerticalList from './VerticalList.jsx';
+// import HorizontalList from './HorizontalList.jsx';
+// import Grid from './Grid.jsx';
+// import Navigation from './Navigation.jsx';
 
-export {
-  Navigation as default,
-  VerticalList,
-  HorizontalList,
-  Grid,
-  Focusable
-};
+// export {
+//   Navigation as default,
+//   VerticalList,
+//   HorizontalList,
+//   Grid,
+//   Focusable
+// };
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
